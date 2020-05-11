4fd290245931a29a2884b7c27e3aba29273d42bf
Martijn Vegter <martijn@martijnvegter.com>
2020-05-11 09:31:50 +0100

    chore: generated java client

--- java/src/main/java/org/openapitools/client/model/ApiInformation.java
+++ java/src/main/java/org/openapitools/client/model/ApiInformation.java
@@ -28,7 +28,7 @@ import java.io.IOException;
  * API information
  */
 @ApiModel(description = "API information")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-05-10T22:06:45.373+01:00[Europe/London]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-05-11T09:31:38.792+01:00[Europe/London]")
 public class ApiInformation {
   public static final String SERIALIZED_NAME_NAME = "name";
   @SerializedName(SERIALIZED_NAME_NAME)
