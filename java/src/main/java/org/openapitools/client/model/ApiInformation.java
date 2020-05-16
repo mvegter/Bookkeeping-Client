c6fb90c05a3dedf979c7c86a22455492c11e3fdf
Martijn Vegter <martijn@martijnvegter.com>
2020-05-16 15:16:58 +0100

    chore: generated java client

--- java/src/main/java/org/openapitools/client/model/ApiInformation.java
+++ java/src/main/java/org/openapitools/client/model/ApiInformation.java
@@ -28,7 +28,7 @@ import java.io.IOException;
  * API information
  */
 @ApiModel(description = "API information")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-05-16T12:00:02.661+01:00[Europe/London]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-05-16T15:16:55.570+01:00[Europe/London]")
 public class ApiInformation {
   public static final String SERIALIZED_NAME_NAME = "name";
   @SerializedName(SERIALIZED_NAME_NAME)
--- java/src/main/java/org/openapitools/client/model/Error.java
+++ java/src/main/java/org/openapitools/client/model/Error.java
@@ -29,7 +29,7 @@ import org.openapitools.client.model.ErrorSource;
  * An Error object.
  */
 @ApiModel(description = "An Error object.")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-05-16T12:00:02.661+01:00[Europe/London]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-05-16T15:16:55.570+01:00[Europe/London]")
 public class Error {
   public static final String SERIALIZED_NAME_STATUS = "status";
   @SerializedName(SERIALIZED_NAME_STATUS)
